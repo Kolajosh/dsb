4d507d7a65f904e401e93de39c0fa747b8c8c74b
Kolawole Joshua <Kolawole.joshua@wemabank.com>
2023-11-05 19:58:37 +0100

    added url for nft

--- src/view/Onboarding/Landing.jsx
+++ src/view/Onboarding/Landing.jsx
@@ -181,6 +181,17 @@ const Landing = () => {
                   }}
                 />
               </div>
+              <div data-aos="fade-up" data-aos-duration="1000">
+                <a
+                  href="https://embed.ipfscdn.io/ipfs/bafybeigdie2yyiazou7grjowoevmuip6akk33nqb55vrpezqdwfssrxyfy/erc1155.html?contract=0xe8f3B5ED742aD599bfEF7B999D7e9ad51833f64a&chain=%7B%22name%22%3A%22Arbitrum+One%22%2C%22chain%22%3A%22ETH%22%2C%22rpc%22%3A%5B%22https%3A%2F%2Farbitrum.rpc.thirdweb.com%2F%24%7BTHIRDWEB_API_KEY%7D%22%5D%2C%22nativeCurrency%22%3A%7B%22name%22%3A%22Ether%22%2C%22symbol%22%3A%22ETH%22%2C%22decimals%22%3A18%7D%2C%22shortName%22%3A%22arb1%22%2C%22chainId%22%3A42161%2C%22testnet%22%3Afalse%2C%22slug%22%3A%22arbitrum%22%2C%22icon%22%3A%7B%22url%22%3A%22ipfs%3A%2F%2FQmcxZHpyJa8T4i63xqjPYrZ6tKrt55tZJpbXcjSDKuKaf9%2Farbitrum%2F512.png%22%2C%22width%22%3A512%2C%22height%22%3A512%2C%22format%22%3A%22png%22%7D%7D&clientId=3185f0af8a8a0a334a5400f8e400fdf7&tokenId=0&theme=dark&primaryColor=purple"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <div className="bg-[#FFFFFF] border-[1.5px] bg-opacity-[10%] border-[#8DA4EA] text-white py-3 cursor-pointer rounded-xl text-xs px-5">
+                    Mint NFT
+                  </div>
+                </a>
+              </div>
             </div>
           </div>
         </div>
